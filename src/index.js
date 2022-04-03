827ca264f71d79ac1f8a25114dd1c40c8f5e1ce8
Leonich77 <leonich777@rambler.ru>
2022-04-03 17:07:38 +0500

    added react-router and their routes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
 import App from './App';
 import 'normalize.css';
+import './index.css';
+import Search from './Search';
 
 // eslint-disable-next-line react/jsx-filename-extension
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="search" element={<Search />} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById('root'),
+);
